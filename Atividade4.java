89de94807cad5de016eae20e0ffebb15fc98d266
KarenESTATEduarda <74926693+KarenESTATEduarda@users.noreply.github.com>
2021-06-27 15:53:28 -0300

    Create Atividade4.java

---
+++ Atividade4.java
@@ -0,0 +1,13 @@
+public  class  TP4 {
+
+    / **
+     * @param args os argumentos da linha de comando
+     * /
+    public  static  void  main ( String [] args ) {
+        matrizes classeArrays =  new classeArrays ();
+
+        matrizes . CalculaDivisaoArrays ();
+
+    }
+
+}
