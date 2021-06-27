2a3762e045bef0f71816beda239aaa165fc5525e
KarenESTATEduarda <74926693+KarenESTATEduarda@users.noreply.github.com>
2021-06-27 15:51:56 -0300

    Create ExcecaoDivisaoResultadoZero.java

---
+++ ExcecaoDivisaoResultadoZero.java
@@ -0,0 +1,7 @@
+public  class  ExcecaoDivisaoResultadoZero  extends  Exception {
+    @Sobrepor
+    public  String  getMessage () {
+        retornar  " O resultado da divisão é 0 " ;
+    }
+    
+}
